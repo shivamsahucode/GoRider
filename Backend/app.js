4f34c3a790aaab9e134fd6191e978adba5397a8e
shivam sahu <shivamsahub77@gmail.com>
2026-09-21 16:23:04 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,25 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 app.get('/',(req,res)=>{
     res.send("hello world");
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
